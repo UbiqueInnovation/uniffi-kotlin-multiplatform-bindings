c4e49f859b64fd4e96c57fc7cc84725115b4602f
Alexey Lebedenko <lebedenko@ubique.ch>
2025-01-15 14:57:07 +0100

    Fix ObjectCleaner on native

--- bindgen/src/templates/native/ObjectCleanerHelper.kt
+++ bindgen/src/templates/native/ObjectCleanerHelper.kt
@@ -1,17 +1,31 @@
 
+{{- self.add_import("kotlinx.atomicfu.atomic") }}
+{{- self.add_import("kotlinx.atomicfu.AtomicBoolean") }}
 {{- self.add_import("kotlinx.coroutines.Runnable") }}
 {{- self.add_import("kotlin.native.ref.createCleaner") }}
 
 private class NativeCleaner : UniffiCleaner {
     override fun register(value: Any, cleanUpTask: Runnable): UniffiCleaner.Cleanable =
-        UniffiNativeCleanable(value, cleanUpTask)
+        UniffiNativeCleanable(OnceRunnable(cleanUpTask))
 }
 
-private class UniffiNativeCleanable(val value: Any, val cleanUpTask: Runnable) : UniffiCleaner.Cleanable {
+private class UniffiNativeCleanable(val cleanUpTask: Runnable) : UniffiCleaner.Cleanable {
     @OptIn(ExperimentalNativeApi::class)
+    private val cleaner = createCleaner(cleanUpTask) {
+        it.run()
+    }
+    
     override fun clean() {
-        createCleaner(this) {
-            it.cleanUpTask.run()
+        cleanUpTask.run()
+    }
+}
+
+private class OnceRunnable(val cleanUpTask: Runnable): Runnable {
+    private val cleaned: AtomicBoolean = atomic(false)
+
+    override fun run() {
+        if (cleaned.compareAndSet(false, true)) {
+            cleanUpTask.run()
         }
     }
 }
