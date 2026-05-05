48fde319ace505d90d8272d2d50dd000e0612a5e
Cléa Benz <benz@ubique.ch>
2026-05-05 15:41:42 +0200

    set minSDK

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -79,6 +79,9 @@ kotlin {
 android {
     namespace = "uniffi.runtime"
     compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
