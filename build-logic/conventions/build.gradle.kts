48260c83691f554bc053430d7ab1b3349f8946e9
Alexey <lebedenko@ubique.ch>
2026-06-29 10:37:01 +0200

    Upgrade to kotlin 2.4.0 (#14)
    * Update to kotlin 2.4.0
    
    * Fix some compiler warnings
    
    * Don't annotate override functions with @Throws

--- build-logic/conventions/build.gradle.kts
+++ build-logic/conventions/build.gradle.kts
@@ -1,15 +1,10 @@
 plugins {
     `kotlin-dsl`
-    alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
     implementation(project(":gradle-plugin"))
     implementation(libs.kotlin.gradle.plugin)
-    // AGP is now compileOnly in :gradle-plugin (so it isn't bundled into the published jar), so
-    // it no longer reaches here transitively. The convention script plugins apply the uniffi
-    // plugin, whose UniffiPlugin references com.android.build.gradle.BaseExtension, so AGP must
-    // be present on this build-logic classpath. This does not affect the published plugin jar.
     implementation(libs.android.tools.gradle)
 
     // https://github.com/gradle/gradle/issues/15383
